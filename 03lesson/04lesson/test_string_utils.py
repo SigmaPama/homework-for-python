f47b33be8482ef5afd7b1376b5507ee0169d7234
SigmaPama <rabota.chuguev@gmail.com>
2025-07-29 22:09:22 +0300

    six commit

--- 03lesson/04lesson/test_string_utils.py
+++ 03lesson/04lesson/test_string_utils.py
@@ -1,28 +1,28 @@
 import pytest
 from stringutils import StringUtils
 
-utils = StringUtils
+utils = StringUtils()
 
 @pytest.mark.parametrize("input_text, excepted_text", [("hi, man", "Hi, man"), 
 ("kinopoisk", "Kinopoisk"), 
-("dear", " ") ],)
+("dear", "Dear ") ],)
 def test_capitalize_positiv_negative (input_text, expected_text):
     assert utils.capitalize(input_text) == expected_text
 
 @pytest.mark.parametrize("input_text, output_text", [("  Hi", "Hi"), 
-("trouble", "  trouble"), ("none", "none"), ('violence', ""),],)
+("  trouble", "trouble"), ("none", "none"), ('violence', ""),],)
 def test_trim_positive_negative (input_text, output_text):
     assert utils.trim(input_text) == output_text
 
 
-@pytest.mark.parametrize("text, letter", [("book", "o"), 
-("Street", "S"), 
-("room", "t")],)
-def test_contains_positive_negative(text, letter):
-    assert utils.contains(text) == letter
+@pytest.mark.parametrize("text, letter, expexted", [("book", "o", True), 
+("Street", "S", True), 
+("room", "t", False)],)
+def test_contains_positive_negative(text, letter, expexted):
+    assert utils.contains(text,letter) == expexted
 
-@pytest.mark.parametrize("input_text, without_symbol", [("trust", "r"),
-("symbol", "m"), 
-("text", "y"), ],)
-def test_exit_positive_negative(input_text, without_symbol):
-    assert utils.delete_symbol(input_text) == without_symbol
+@pytest.mark.parametrize("input_text, symbol, without_symbol ", [("trust", "r", "rust"),
+("symbol", "m", "symbol"), 
+("text", "y"), "text" ],)
+def test_delete_symbol_positive_negative(input_text, symbol, without_symbol):
+    assert utils.delete_symbol(input_text, symbol) == without_symbol
