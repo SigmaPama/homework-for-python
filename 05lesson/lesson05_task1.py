be79f52a94d318023b48f04601e8ebc044b1ad41
SigmaPama <rabota.chuguev@gmail.com>
2025-08-04 15:38:03 +0300

    seventh commit

--- 05lesson/lesson05_task1.py
+++ 05lesson/lesson05_task1.py
@@ -5,11 +5,12 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+print(ChromeDriverManager().install())
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome()
 
 driver.get("http://uitestingplayground.com/classattr")
-driver.findElement(By.cssSelector("button.btn-primary")).click()
+driver.find_element(By.CSS_SELECTOR, ("button.btn-primary")).click()
 
 sleep(5)
 
--- 05lesson/lesson05_task3.py
+++ 05lesson/lesson05_task3.py
@@ -1,5 +1,6 @@
 from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
@@ -7,7 +8,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 driver.get("http://the-internet.herokuapp.com/inputs")
-search_box = driver.find_element(By.CSS_SELECTOR, type="number")
+search_box = driver.find_element(By.CSS_SELECTOR, "[type='number']")
 search_box.send_keys("Sky")
 search_box.clear()
 search_box.send_keys("Pro")
