cf534e88e4b02c2ddfe06fc752d69b43585f0a13
SigmaPama <rabota.chuguev@gmail.com>
2025-08-04 15:40:29 +0300

    seventh commit

--- 05lesson/lesson05_task2.py
+++ 05lesson/lesson05_task2.py
@@ -4,8 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+print(ChromeDriverManager().install())
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome()
 
 driver.get("http://uitestingplayground.com/dynamicid")
 blue_click = driver.find_element(By.CSS_SELECTOR, "button.btn")
