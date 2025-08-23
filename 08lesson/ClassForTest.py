89f5441a7f7bb7ce57fffc3e83fe5a20d2162a84
SigmaPama <rabota.chuguev@gmail.com>
2025-08-23 16:38:13 +0300

    eleventh commit

--- 08lesson/ClassForTest.py
+++ 08lesson/ClassForTest.py
@@ -4,8 +4,8 @@ class base_option:
 
     def __init__(self, url):
         self.base_url = url
-        self.login = "rabota.chuguev@gmail.com"
-        self.password = "Uoj067ap"
+        self.login = "real_login_here"
+        self.password = "real_password_here"
         self.response = None 
 
     def companny_list(self):
