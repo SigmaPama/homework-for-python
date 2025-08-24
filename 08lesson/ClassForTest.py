5cd87b3491cb4b3a864bbf3c88058c7c026d4709
SigmaPama <rabota.chuguev@gmail.com>
2025-08-24 19:52:00 +0300

    eleventh commit

--- 08lesson/ClassForTest.py
+++ 08lesson/ClassForTest.py
@@ -1,16 +1,17 @@
 import requests
 
+
 class base_option:
 
     def __init__(self, url):
         self.base_url = url
-        self.login = "real_login_here"
-        self.password = "real_password_here"
-        self.response = None 
-
+        self.login = "rabota.chuguev@gmail.com"
+        self.password = "Uoj067ap"
+        self.response = None
+        self.token = None
 
     def companny_list(self):
-        url = self.base_url +"/api-v2/auth/companies"
+        url = self.base_url + "/api-v2/auth/companies"
         payload = {
         'login': self.login,
         'password': self.password,
@@ -19,16 +20,24 @@ class base_option:
         self.response = requests.post(url, json=payload, headers=headers)
         return self.response.json()["content"][0]['id']
 
-
-
     def get_token(self):
-        url = self.base_url +"/api-v2/auth/keys/get"
+        url = self.base_url + "/api-v2/auth/keys/get"
         payload = {
             'login': self.login,
             'password': self.password,
-            'companyId': self.response.json()["content"][0]["id"]
-        }
+            'companyId': self.response.json()["content"][0]["id"]}
         headers = {'Content-Type': 'application/json'}
         result = requests.post(url, json=payload, headers=headers)
-        res_id = result.json()
-        return res_id[0]["key"]
+        self.res_id = result.json()
+        return self.res_id[0]["key"]
+    
+    def get_new_project(self, title):
+        url = self.base_url + "/api-v2/projects"
+        my_headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {self.res_id[0]["key"]}'}
+        body = {
+        "title": title,
+        "users": {}}
+        response = requests.post(url, json=body, headers=my_headers)
+        return response.json()["id"]
--- 08lesson/test_what.py
+++ 08lesson/test_what.py
@@ -1,67 +1,60 @@
-import pytest
 import requests
 from ClassForTest import base_option
 
 
-#"id": "d15204b2-4309-4f23-90ae-67b6a404e626",
-#"name": "Поток_98.2",
-#"isAdmin": true
 base_url = "https://ru.yougile.com"
 api = base_option("https://ru.yougile.com")
 
 
-
-
-def test_create_company():
+def test_create_project():
     id = api.companny_list()
     token = api.get_token()
     my_headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {token}'
     }
-
     body = {
         "title": "NextDoor",
         "users": {}
     }
-    my_new_company = requests.post(base_url+"/api-v2/projects", json=body, headers=my_headers)
-    my_new_company.json()
-    assert id == my_new_company.json()['id'] #Негативная
-    assert my_new_company.status_code == 201
-    assert my_new_company.json()["title"] == "Nextdoor" #Негативная
+    my_new_project = requests.post(base_url+"/api-v2/projects",
+    json=body, headers=my_headers)
+    my_new_project.json()
+    assert my_new_project.status_code == 201
+    assert id != my_new_project.json()["id"]
+    
+    
 
-def test_change_in_company():
-    id = api.companny_list()
+def test_change_in_project():
+    api.companny_list()
     token = api.get_token()
+    project = api.get_new_project("BOOO")
     body = {
         "deleted": True,
-        "title": "Поток_98.2",
+        "title": "BOOO",
         "users": {}
     }
-    
-    my_headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer +{token}'
-    }
-
-    changed_company_id = requests.put(base_url+ f"/api-v2/projects/{id}", json=body, headers=my_headers)
-    changed_company_id.json()
-    assert changed_company_id.status_code == 200
-    assert id == changed_company_id.json()['id']
-    assert changed_company_id.json()["deleted"] == False #Негативная
-
-
-
-def test_get_with_id():
-    
-    id = api.companny_list()
-    token = api.get_token()
     my_headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {token}'
     }
+    changed_company_id = requests.put(base_url + f"/api-v2/projects/{project}",
+    json=body, headers=my_headers)
+    changed_company_id.json()
+    assert changed_company_id.status_code == 200
+    assert project == changed_company_id.json()['id']
 
-    get_id = requests.get(base_url+ f"/api-v2/projects/{id}", headers=my_headers)
+
+def test_get_with_id():  
+    api.companny_list()
+    token = api.get_token()
+    project = api.get_new_project("New one")
+    my_headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+    get_id = requests.get(base_url + f"/api-v2/projects/{project}",
+    headers=my_headers)
     get_id.json()
     assert get_id.status_code == 200
-    assert get_id.json()["id"] == None #Негативная
+    assert get_id.json()["id"] == project
